15ce776f77b1798cfaa394fb23d61c9075ec34a0
Steam Labs <care@steamlabs.in>
2017-06-27 17:32:46 +0530

    Update LCD_Shield.ino

--- LCD_Shield.ino
+++ LCD_Shield.ino
@@ -1,6 +1,6 @@
 /*
  Base Code for LCD Shield
- Developed by- Harkrishan Singh (harkrishan@steamlabs.in)
+ Tested & Improvised by - Harkrishan Singh (harkrishan@steamlabs.in)
 */
 
 #include <LiquidCrystal.h>
